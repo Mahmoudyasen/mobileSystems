a1eee1d78f1bfaf792bffff47016729abe351a12
mahmoud yaseen S.YASEEN <128331104+Mahmoudyasen@users.noreply.github.com>
2024-11-15 15:27:30 +0300

    Update TextScreen.js

--- TextScreen.js
+++ TextScreen.js
@@ -27,3 +27,35 @@ const styles = StyleSheet.create({
         borderWidth: 2,
     },
 });
+
+//to make it with the more than 5 reminder
+import { StyleSheet, Text, View , TextInput} from 'react-native'
+import React , {useState} from 'react'
+
+const TextScreen = () => {
+    const [text, setText] = useState("");
+  return (
+    <View>
+      <Text>TextScreen</Text>
+      <TextInput 
+      autoCapitalize='none'
+      autoCorrect={false}
+      style={styles.input}
+      value={text}
+      onChangeText={(newValue) => setText(newValue)}
+      />
+      <Text>your text is: {text}</Text>
+      {text.length < 5 ? <Text>type more than 5 ch</Text> : null}
+    </View>
+  )
+}
+
+export default TextScreen
+
+const styles = StyleSheet.create({
+    input:{
+        margin: 15,
+        borderColor: "red",
+        borderWidth: 2,
+    },
+});
